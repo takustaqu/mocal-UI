5c0a88c6ceef04e71d760d25a6aabf9afd5b7bbd
Yabu Kiyohide <takustaqu@ow-d.com>
2014-11-16 12:20:08 +0900

    autocomplete-style

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,11 @@
 
 //Search-form
+var query = "";
+
+
+$(function(){
+
+
 (function(){
 
 	$searchInput = $("#search-input");	
@@ -15,28 +21,96 @@
 		}
 	})
 
-	$("#search-submit").click(function(){
-		console.log("foo")
-		
-		$(".mainvis").slideUp("normal","swing",function(){
-			$("#result").find(".card").each(function(i){
-				var $target = $(this)
-				
-				setTimeout(function(){
-					$target.addClass("active");
-				},i*100)
-				console.log(i);
+	
+
+	$("#search-form").on({
+		"submit":function(){
+			$(".mainvis").slideUp("normal","swing",function(){
+				$("#result").find(".card").each(function(i){
+					var $target = $(this)
+					setTimeout(function(){
+						$target.addClass("active");
+					},i*100)
+					console.log(i);
+				});
+
+				query = encodeURI($("#search-input").val());
+			})
+
+
+
+			$.ajax({
+			  type: "GET",
+			  url: "http://mocal.cloudapp.net/api/asahi?q="+encodeURI($("#search-input").val()),
+			  dataType: 'json',
+			  success: function(json){
+			  	console.log(json)
+			  	var $asahi = $("#asahi")
+			  	$asahi.find(".positive-article").append(render.asahiArticle(json.positiveArticle))
+			  	$asahi.find(".negative-article").append(render.asahiArticle(json.negativeArticle))
+			  	$asahi.find(".positive-word").append(render.asahiWords(json.positiveWord))
+			  	$asahi.find(".negative-word").append(render.asahiWords(json.negativeWord))
+			    
+			  }
 			});
-		})
-		return false;
+
+			$.ajax({
+			  type: "GET",
+			  url: "http://mocal.cloudapp.net/api/companyDetail?q="+encodeURI($("#search-input").val()),
+			  dataType: 'json',
+			  success: function(json){
+			  	var $result = $("#result")
+				var $ul = $("<ul />").addClass()
+				$("#result").append("foo");
+				console.log($("#result").find(".name"))
+				$ul.append('<li class="name">'+ json.name +' <small class="badge">証券番号:'+ json.stock_code +'</small></li>');
+				$ul.append('<li class="stockPrice">&yen;'+_c.devideComma(json.price)+' <small class="timestamp">0000/00/00 00:00現在</small></li>');
+				$ul.append('<li><div class="">▼</div></li>');
+				$("#overview .card").append($ul);
+			  }
+			});
+
+			$.ajax({
+			  type: "GET",
+			  url: "http://mocal.cloudapp.net/api/social?q="+encodeURI($("#search-input").val()),
+			  dataType: 'json',
+			  success: function(json){
+			  	//{positiveComment: null, negativeComment: null, socialTrend: Object}
+			  	console.log(json)
+			  	// $asahi.find(".positive-article").append(render.asahiArticle(json.positiveArticle))
+			  	// $asahi.find(".negative-article").append(render.asahiArticle(json.negativeArticle))
+			  	// $asahi.find(".positive-word").append(render.asahiWords(json.positiveWord))
+			  	// $asahi.find(".negative-word").append(render.asahiWords(json.negativeWord))
+			    
+			  }
+			});
+
+
+			$("#judgements").find(".graph").each(function(){
+			var geom = {
+			w:$(this).children("img").width(),
+			h:$(this).children("img").height()
+			}
+			$(this).children("img").css("opacity",0)
+			$(this).children("canvas").width(geom.w).width(geom.h).css({"top":0,"left":0,"position":"absolute"})
+			})
+			var doughnutData = [
+			{value: 30,color:"#aaf2fb"},
+			{value: 50,color: "#ffb6b9"},
+			{value: 120,color: "#ffe361"},
+			{value: 170,color: "#fbaa6e"},
+			{value: 70,color: "#A8BECB"}];
+
+
+			$("canvas.chart").each(function(){
+			var myDoughnut = new Chart($(this)[0].getContext("2d")).Doughnut(doughnutData);
+			})
+
+			return false;
+		}
 	})
 
 })()//Search-form
-
-
-$(function(){
-
-
 
 
 var render = {
@@ -49,77 +123,7 @@ var render = {
 		}		
 	}
 
-var query = encodeURI("ぐるなび");
 
-
-// $.ajax({
-//   type: "GET",
-//   url: "http://mocal.cloudapp.net/api/asahi?q="+query,
-//   dataType: 'jsonp',
-//   success: function(json){
-//   	var $asahi = $("#asahi")
-//   	$asahi.find(".positive-article").append(render.asahiArticle(json.positiveArticle))
-//   	$asahi.find(".negative-article").append(render.asahiArticle(json.negativeArticle))
-//   	$asahi.find(".positive-word").append(render.asahiWords(json.positiveWord))
-//   	$asahi.find(".negative-word").append(render.asahiWords(json.negativeWord))
-    
-//   }
-// });
-
-$.ajax({
-  type: "GET",
-  url: "http://mocal.cloudapp.net/api/companyDetail?q="+query,
-  dataType: 'json',
-  success: function(json){
-  	var $overview = $("#overview").find(".card").eq(0);
-
-  	console.log(json);
-  	//{name: "ぐるなび", stock_code: 2440, price: 1689} 
-	
-	$("#overview").find(".name").text('<li class="name">'+ json.name +' <small class="badge">証券番号:'+ json.stock_code +'</small></li>')
-	$("#overview").find(".stockPrice").text('<li class="stockPrice">&yen;'+_c.devideComma(json.price)+' <small class="timestamp">0000/00/00 00:00現在</small></li>')
-	$("#overview").find(".vector").text('<div class="">▼</div>')
-  }
-});
-
-$.ajax({
-  type: "GET",
-  url: "http://mocal.cloudapp.net/api/social?q="+query,
-  dataType: 'json',
-  success: function(json){
-  	//{positiveComment: null, negativeComment: null, socialTrend: Object}
-  	console.log(json)
-  	// $asahi.find(".positive-article").append(render.asahiArticle(json.positiveArticle))
-  	// $asahi.find(".negative-article").append(render.asahiArticle(json.negativeArticle))
-  	// $asahi.find(".positive-word").append(render.asahiWords(json.positiveWord))
-  	// $asahi.find(".negative-word").append(render.asahiWords(json.negativeWord))
-    
-  }
-});
-
-
-
-
-
-$("#judgements").find(".graph").each(function(){
-var geom = {
-w:$(this).children("img").width(),
-h:$(this).children("img").height()
-}
-$(this).children("img").css("opacity",0)
-$(this).children("canvas").width(geom.w).width(geom.h).css({"top":0,"left":0,"position":"absolute"})
-})
-var doughnutData = [
-{value: 30,color:"#aaf2fb"},
-{value: 50,color: "#ffb6b9"},
-{value: 120,color: "#ffe361"},
-{value: 170,color: "#fbaa6e"},
-{value: 70,color: "#A8BECB"}];
-
-
-$("canvas.chart").each(function(){
-var myDoughnut = new Chart($(this)[0].getContext("2d")).Doughnut(doughnutData);
-})
 
 
 
