bf0ffba78d8563a1a7ed0a1f69d89c578a454dfd
Yabu Kiyohide <takustaqu@ow-d.com>
2014-11-16 11:22:29 +0900

    恥部の消去

--- scripts/main.js
+++ scripts/main.js
@@ -34,6 +34,10 @@
 })()//Search-form
 
 
+$(function(){
+
+
+
 
 var render = {
 		asahiArticle:function(input){
@@ -44,8 +48,6 @@ var render = {
 			return "<ol><li>"+input[0]+"</li><li>"+input[1]+"</li><li>"+input[2]+"</li><li>"+input[3]+"</li><li>"+input[4]+"</li></ol>"
 		}		
 	}
-
-
 
 var query = encodeURI("ぐるなび");
 
@@ -69,25 +71,14 @@ $.ajax({
   url: "http://mocal.cloudapp.net/api/companyDetail?q="+query,
   dataType: 'json',
   success: function(json){
-  	var $overview = $("#overview");
+  	var $overview = $("#overview").find(".card").eq(0);
 
   	console.log(json);
   	//{name: "ぐるなび", stock_code: 2440, price: 1689} 
-
-
-<ul class="negative">
-				<li class="name">セイコーエプソン <small class="badge">証券番号:0000</small></li>
-				<li class="stockPrice">&yen;00,000 <small class="timestamp">0000/00/00 00:00現在</small></li>
-				<li class="vector">
-					<div class="">▼</div>
-				</li>
-			</ul>
-
-  	$overview.find(".name").append(render.asahiArticle(json.positiveArticle))
-  	$overview.find(".negative-article").append(render.asahiArticle(json.negativeArticle))
-  	$overview.find(".positive-word").append(render.asahiWords(json.positiveWord))
-  	$overview.find(".negative-word").append(render.asahiWords(json.negativeWord))
-    
+	
+	$("#overview").find(".name").text('<li class="name">'+ json.name +' <small class="badge">証券番号:'+ json.stock_code +'</small></li>')
+	$("#overview").find(".stockPrice").text('<li class="stockPrice">&yen;'+_c.devideComma(json.price)+' <small class="timestamp">0000/00/00 00:00現在</small></li>')
+	$("#overview").find(".vector").text('<div class="">▼</div>')
   }
 });
 
@@ -110,39 +101,26 @@ $.ajax({
 
 
 
-		$("#judgements").find(".graph").each(function(){
-			var geom = {
-				w:$(this).children("img").width(),
-				h:$(this).children("img").height()
-				}
-			$(this).children("img").css("opacity",0)
-			$(this).children("canvas").width(geom.w).width(geom.h).css({"top":0,"left":0,"position":"absolute"})
-		})
+$("#judgements").find(".graph").each(function(){
+var geom = {
+w:$(this).children("img").width(),
+h:$(this).children("img").height()
+}
+$(this).children("img").css("opacity",0)
+$(this).children("canvas").width(geom.w).width(geom.h).css({"top":0,"left":0,"position":"absolute"})
+})
 var doughnutData = [
-　　{
-　　　value: 30,
-　　　color:"#aaf2fb"
-　　},
-　　{
-　　　value: 50,
-　　　color: "#ffb6b9"
-　　},
-　　{
-　　　value: 120,
-　　　color: "#ffe361"
-　　},
-   {
-　　　value: 170,
-　　　color: "#fbaa6e"
-　　},
-   {
-　　　value: 70,
-　　　color: "#A8BECB"
-　　}
-];
+{value: 30,color:"#aaf2fb"},
+{value: 50,color: "#ffb6b9"},
+{value: 120,color: "#ffe361"},
+{value: 170,color: "#fbaa6e"},
+{value: 70,color: "#A8BECB"}];
 
 
 $("canvas.chart").each(function(){
-	var myDoughnut = new Chart($(this)[0].getContext("2d")).Doughnut(doughnutData);
-
+var myDoughnut = new Chart($(this)[0].getContext("2d")).Doughnut(doughnutData);
 })
+
+
+
+})//ready
